8efb0dcc5375201d69d1880f798ea58f33ecede2
ahmed aboukoura <ahmedaboukoura1993@gmail.com>
2017-04-04 00:58:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  result = true
+  counter = 2
+    while counter < num
+      if num % counter ==0
+        result = false
+    end
+        counter +=1
+  end
+    if  num < 2
+      return false
+    else
+     return result
+   end
+end
